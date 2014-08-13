d7dbbf1277d58c5129418d5b54d33946292623cf
fernandosoria <soria.fernando@gmail.com>
2014-08-13 23:06:28 +0000

    Minor update to User controller to allow user to update name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,19 @@ class UsersController < ApplicationController
   def show
     @wikis = current_user.wikis
   end
+
+  def update
+    if current_user.update_attributes(user_params)
+      flash[:notice] = "User information updated"
+      redirect_to edit_user_registration_path
+    else
+      render "devise/registrations/edit"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
 end
